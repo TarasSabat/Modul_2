5f84e4f56e587fc230558063c7c1e4452908476b
TarasSabat <tarsab25@gmail.com>
2023-09-10 00:08:12 +0300

    1

--- konspekt.py
+++ konspekt.py
@@ -120,3 +120,70 @@
 # for char in fruit:
 #     print(char)
 
+# # Цикл while
+# a = 1
+# while a <= 5:
+#     print(a)
+#     a = a + 1
+
+# # «Нескінченні цикли» та break
+# a = 0
+# while True:
+#     print(a)
+#     if a >= 20:
+#         break
+#     a = a + 1
+
+# # Наприклад echo скрипт, який виводить в консоль те, що ви введете, доки ви не введете exit:
+# while True:
+#     user_input = input()
+#     print(user_input)
+#     if user_input == "exit":
+#         break
+
+# # Завершення ітерації за допомогою continue
+# a = 0
+# while a < 6:
+#     a = a + 1
+#     if not a % 2:
+#         continue
+#     print(a)
+
+# # Оператори continue та break працюють тільки всередині одного циклу. 
+# # В ситуації вкладених циклів немає способу вийти з усіх циклів одразу.
+# while True:
+#     number = input("number = ")
+#     number = int(number)
+#     while True:
+#         print(number)
+#         number = number - 1
+#         if number < 0:
+#             break
+
+# # Також використання continue або break поза циклом призводить до синтаксичної помилки.
+# number = int(input("number = "))
+# if number < 0:
+#     break
+
+# # Винятки
+# val = 'a'
+# try:
+#     val = int(val)
+# except ValueError:
+#     print(f"val {val} is not a number")
+# else:
+#     print(val > 0)
+# finally:
+#     print("This will be printed anyway")
+
+# Механізм обробки винятків
+age = input("How old are you? ")
+# try:
+#     age = int(age)
+#     if age >= 18:
+#         print("You are adult.")
+#     else:
+#         print("You are infant")
+# except ValueError:
+#     print(f"{age} is not a number")
+
