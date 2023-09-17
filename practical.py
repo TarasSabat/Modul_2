208bf752064427602e1ff0a266492aa2c797951d
TarasSabat <tarsab25@gmail.com>
2023-09-17 20:57:41 +0300

    1

--- practical.py
+++ practical.py
@@ -1,5 +1,7 @@
-# # Введення пін-кода (3 спроби)
-# # Через while
+'''
+Введення пін-кода (3 спроби)
+Через while
+'''
 # pin = '0123'
 # n = 0
 # max_tries = 3
@@ -31,8 +33,9 @@
 #         n += 1
 
 # print('Good bye!')
-
-# # Через for
+'''
+Через for
+'''
 # pin = '0123'
 # for i in range(3):   
 #     user_pin = input('Enter your pin: ')
@@ -45,9 +48,9 @@
 #     else:
 #         print('Pin should be 4 or 6 digits.')
 # print('Good bye!')
-
-
-## Перевірка чи 3-и значне чичсло поліндром (зліва направа і зправа на ліво читається однаково)
+'''
+Перевірка чи 3-и значне чичсло поліндром (зліва направа і зправа на ліво читається однаково)
+'''
 # num = int(input('Number: '))
 
 # n1 = num // 100
@@ -58,9 +61,9 @@
 
 # else: 
 #     print('No')
-
-# # Перевірити чи в 3-х значному числі всі цифри різні? І чи є тільки 2 однакових числа?
-
+'''
+Перевірити чи в 3-х значному числі всі цифри різні? І чи є тільки 2 однакових числа?
+'''
 # num = input("Number: ")
 
 # if len(num) != 3:
@@ -75,9 +78,10 @@
         
 #     elif (n1 == n3 or n2 == n3 or n2 == n1) and not (n1 == n2 and n1 == n3):
 #         print('Є тільки дві однакові цифри')
-
-# # Скласти програму, яка вводить цілі k, l.
-# # Якщо вони не рівні, то менше з них замінюється більшим, інакше обидва замінюються на 0.
+'''
+Скласти програму, яка вводить цілі k, l.
+Якщо вони не рівні, то менше з них замінюється більшим, інакше обидва замінюються на 0.
+'''
 # k = int(input("Введіть число k: "))
 # l = int(input("Введіть число l: "))
 
@@ -129,8 +133,9 @@
 
 # a, b = b, a
 # print(f'a: {a}, b: {b}')
-
-# # Знайти у рядку номер першого входження символу 'a'
+'''
+Знайти у рядку номер першого входження символу 'a'
+'''
 # string = input("Введіть строку: ")
 
 # count = 0
@@ -141,8 +146,9 @@
 #     count += 1
 
 # print(f"a: {count}")
-
-# # Знайдемо індекс останнього входження символу 'а'
+'''
+Знайдемо індекс останнього входження символу 'а'
+'''
 # string = input('String: ')
 
 # count = 0
@@ -153,8 +159,9 @@
 #     count += 1
 
 # print(f'The latest a is: {index}')
-
-# # Визначити до якої пори року відноситься місяць
+'''
+Визначити до якої пори року відноситься місяць
+'''
 # month = 11
 
 # match month:
@@ -167,10 +174,10 @@
 #     case 12 | 1 | 2:
 #         season = 'зима'
 # print(season)
-
-# # У банк поклали 100 доларів під 5 % річних.
-# # Скласти програму обчислення кількості грошей на рахунку через 10 років. (складні щомісячні відсотки)
-
+'''
+У банк поклали 100 доларів під 5 % річних.
+Скласти програму обчислення кількості грошей на рахунку через 10 років. (складні щомісячні відсотки)
+'''
 # deposit = 100
 # percent = 0.05
 # T = 10
@@ -182,9 +189,9 @@
 # # 164.700949769028
 # # 164.700949769028
 # # print(deposit * (1 + percent / 12) ** (12 * 10))
-
-# # Розрахуємо найбільший спільний дільник для двох цілих чисел з використанням циклу
-
+'''
+Розрахуємо найбільший спільний дільник для двох цілих чисел з використанням циклу
+'''
 # first = int(input("Enter the first integer: "))
 # second = int(input("Enter the second integer: "))
 
@@ -192,63 +199,59 @@
 # while not (first % gcd == 0 and second % gcd == 0):
 #     gcd = gcd - 1
 # print(gcd)
-
+'''
 # Визначити, чи є введений користувачем рядок паліндромом (пробіли не враховуємо)
-line = input("Введіть рядок: ")
-????
-# num = int(input('Number: '))
+'''
+## Варіант 1
+# word= input("Введіть рядок: ")
+# word2 = word[::-1]
+# if word == word2:
+#     print(True)
+# else:
+#     print(False)
 
-# n1 = num // 100
-# n2 = num % 10
+## Варіант 2
+# word = input("Enter word: ")
+# print(word == ''.join(reversed(word)))
 
-# if n1 == n2:
-#     print('Yes')
+## Варіант 3
+# word = input("Please enter word: ").lower()
+# while True:
+#     word = input("Please enter word: ").lower()
+#     if len(word) % 2 == 0:
+#         print ("This word cant be polindrom")
+#         continue
+#     for i, d in enumerate(word):
+#        #print(word[i], word[-i -1], i)
+#         if word[i] != word[-i -1]:
+#            pol = False
+#     break
 
-# else: 
-#     print('No')
+## Варіант 4
+# pol = True
+# while True:
+#     word = input("Please enter word: ").lower()
+#     if len(word) % 2 == 0:
+#         print ("This word cant be polindrom")
+#         continue
+#     for i  in  range(len(word)):
+#        #print(word[i], word[-i -1], i)
+#         if word[i] != word[-i -1]:
+#            pol = False
+#     break
+# print(pol)
 
 ##
-number = 100
-figure = 63
-c = figure % 2 == 0
-if c:
-    for i in range(0,number+1,2):
-        if i == figure:
-            print("Parne", i)
-            break
-else:
-    for i in range(1,number+1,2):
-        if i == figure:
-            print(i)
-            break
-
-##
-word = input("Enter word: ")
-print(word == ''.join(reversed(word)))
-
-##
-word = input("Please enter word: ").lower()
-while True:
-    word = input("Please enter word: ").lower()
-    if len(word) % 2 == 0:
-        print ("This word cant be polindrom")
-        continue
-    for i, d in enumerate(word):
-       #print(word[i], word[-i -1], i)
-        if word[i] != word[-i -1]:
-           pol = False
-    break
-##
-pol = True
-while True:
-    word = input("Please enter word: ").lower()
-    if len(word) % 2 == 0:
-        print ("This word cant be polindrom")
-        continue
-    for i  in  range(len(word)):
-       #print(word[i], word[-i -1], i)
-        if word[i] != word[-i -1]:
-           pol = False
-    break
-print(pol)
-
+# number = 100
+# figure = 63
+# c = figure % 2 == 0
+# if c:
+#     for i in range(0,number+1,2):
+#         if i == figure:
+#             print("Parne", i)
+#             break
+# else:
+#     for i in range(1,number+1,2):
+#         if i == figure:
+#             print(i)
+#             break
